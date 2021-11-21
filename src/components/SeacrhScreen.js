c0683ec3b03cb928bacff58ca3d4e3c42531fac3
Jenny Alexandra Montoya Cortés <30633488+jennymontoya1001@users.noreply.github.com>
2021-11-21 18:57:59 -0500

    movies sin buscador

---
+++ src/components/SeacrhScreen.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export const SeacrhScreen = () => {
+    return (
+        <div>
+              <h1>Search Screen</h1>
+            <hr />
+            <div className="row">
+                <div className="col-5">
+                    <h4> Search Form </h4>
+                    <hr />
+                    <form>
+                        <input 
+                            type="text"
+                            placeholder="Find your hero"
+                            className="form-control"
+                            name="searchText"
+                            autoComplete="off"
+                        />
+                        <button
+                            type="submit"
+                            className="btn m-1 btn-block btn-outline-primary"
+                        >
+                            Search...
+                        </button>
+                    </form>
+                </div>
+                <div className="col-7">
+                    <h4> Results </h4>
+                    <hr />
+                </div>
+            </div>
+
+        </div>
+    )
+}
